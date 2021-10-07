770415d084b91594f64a8fcca41149785321d035
BinaryRealm <48455051+BinaryRealm@users.noreply.github.com>
2021-10-06 20:00:36 -0400

    update auth class

--- lib/auth_class.dart
+++ lib/auth_class.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FireAuthClass {
   static Future<User?> loginEmailPassword(
@@ -43,6 +44,17 @@ class FireAuthClass {
       print(e);
     }
     return user;
+  }
+
+  static Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   static Future<User?> loginAnon() async {
